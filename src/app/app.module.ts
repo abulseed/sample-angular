81bc9b2c4281cd5f8613fe9307eb54c563eeb5ff
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-10-08 17:17:29 +0200

    Added a styled users list header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LogoComponent } from './logo/logo.component';
+import { UsersListHeaderComponent } from './users-list-header/users-list-header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LogoComponent
+    LogoComponent,
+    UsersListHeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
