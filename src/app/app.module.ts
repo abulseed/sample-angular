b08314a6d54f4d943eff28a470e1d75499fc72ab
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-10-09 08:34:50 +0200

    declare new NG components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,37 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LogoComponent } from './logo/logo.component';
 import { UsersListHeaderComponent } from './users-list-header/users-list-header.component';
+import { UserPanelComponent } from './user-panel/user-panel.component';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
+import { EditButtonComponent } from './edit-button/edit-button.component';
+import { DeleteButtonComponent } from './delete-button/delete-button.component';
+import { DetailsButtonComponent } from './details-button/details-button.component';
+import { UserPanelHeadingComponent } from './user-panel-heading/user-panel-heading.component';
+import { UsersListContentComponent } from './users-list-content/users-list-content.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+import { AddUserBoxComponent } from './add-user-box/add-user-box.component';
+import { EditUserBoxComponent } from './edit-user-box/edit-user-box.component';
+import { DeleteUserBoxComponent } from './delete-user-box/delete-user-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LogoComponent,
-    UsersListHeaderComponent
+    UsersListHeaderComponent,
+    UserPanelComponent,
+    UserAvatarComponent,
+    EditButtonComponent,
+    DeleteButtonComponent,
+    DetailsButtonComponent,
+    UserPanelHeadingComponent,
+    UsersListContentComponent,
+    UserInfoComponent,
+    LoginFormComponent,
+    AddUserBoxComponent,
+    EditUserBoxComponent,
+    DeleteUserBoxComponent
   ],
   imports: [
     BrowserModule,
