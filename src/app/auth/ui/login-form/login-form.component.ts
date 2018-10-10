eadcfb24b0dee2cf1734e0ece1f28f13bfcf710b
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-10-11 00:35:49 +0200

    UI enhancements for login component

--- src/app/auth/ui/login-form/login-form.component.ts
+++ src/app/auth/ui/login-form/login-form.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { AuthenticationService } from '../../authentication.service';
 
 @Component({
   selector: 'app-login-form',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
+  msg = '';
 
-  constructor() { }
+  constructor(private authService: AuthenticationService) { }
 
   ngOnInit() {
+  }
+
+  onLogin(form: NgForm) {
+    const email = form.value.email;
+    const password = form.value.password;
+    this.authService.loginUser(email, password).subscribe((token) => {
+      console.log('Login Success:', token);
+    }, (msg: string) => {
+      this.msg = msg;
+      console.log(this.msg);
+    });
   }
 
 }
