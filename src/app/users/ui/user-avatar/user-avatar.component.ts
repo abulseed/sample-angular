ad9f7368ffb8c3f22ea287de8957c9eaa06291a8
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-10-11 00:40:28 +0200

    UI: user avatar

--- src/app/users/ui/user-avatar/user-avatar.component.ts
+++ src/app/users/ui/user-avatar/user-avatar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-avatar',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-avatar.component.scss']
 })
 export class UserAvatarComponent implements OnInit {
+  @Input() avatarSrc: string;
+  @Input() altText: string;
+  @Input() width: string;
+  @Input() height: string;
 
   constructor() { }
 
