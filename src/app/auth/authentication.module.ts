7d2179d981529665b4f769662aca24ead607c40d
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-10-11 00:35:07 +0200

    Added Auth service

--- src/app/auth/authentication.module.ts
+++ src/app/auth/authentication.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { LoginFormComponent } from './ui/login-form/login-form.component';
+import { AlertComponent } from './ui/alert/alert.component';
+import { AuthInterceptor } from './authentication.interceptor';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  exports: [
+    AlertComponent
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   ],
   declarations: [
-    LoginFormComponent
+    LoginFormComponent,
+    AlertComponent
   ]
 })
 export class AuthenticationModule { }
--- src/app/auth/authentication.service.ts
+++ src/app/auth/authentication.service.ts
@@ -1,9 +1,45 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
+import { throwError } from 'rxjs/index';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  serviceUrl = 'https://reqres.in/api/login';
+  token: string;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
+
+  private handleError() {
+    return throwError(
+      'Something bad happened; please try again later.');
+  }
+
+  loginUser(username: string, password: string) {
+    return this.httpClient.post<string>(this.serviceUrl, {
+      'email': username,
+      'password': password
+    }).pipe(
+      catchError(this.handleError),
+      tap((token) => {
+        this.token = token;
+        this.router.navigate(['/users']);
+      })
+    );
+  }
+
+  logoutUser() {
+    this.token = null;
+  }
+
+  getToken() {
+    return this.token;
+  }
+
+  isAuthenticated() {
+    return this.token != null;
+  }
 }
