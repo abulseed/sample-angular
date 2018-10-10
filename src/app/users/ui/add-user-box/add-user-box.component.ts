0cea73eff2c86dd31303e6de21f33523f39dad27
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-10-11 00:39:14 +0200

    Add user box final

--- src/app/users/ui/add-user-box/add-user-box.component.ts
+++ src/app/users/ui/add-user-box/add-user-box.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsersService } from '../../users.service';
 
 @Component({
   selector: 'app-add-user-box',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-user-box.component.scss']
 })
 export class AddUserBoxComponent implements OnInit {
+  msg: string;
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    const username = form.value.name;
+    const jobTitle = form.value.jobTitle;
+    this.usersService.addUser(username, jobTitle).subscribe((response) => {
+      console.log('Add Success');
+      this.msg = 'Success';
+    }, (errorMsg: string) => {
+      this.msg = errorMsg;
+      console.log(this.msg);
+    });
+  }
 }
