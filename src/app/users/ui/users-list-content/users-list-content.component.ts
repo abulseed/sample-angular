e8a801c324c4e342ce0d5a5840e7058e0cd0c780
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-10-11 00:41:12 +0200

    UI: users list

--- src/app/users/ui/users-list-content/users-list-content.component.ts
+++ src/app/users/ui/users-list-content/users-list-content.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../user.model';
+import { UsersService } from '../../users.service';
+import { NotificationService } from '../../../notification/notification.service';
 
 @Component({
   selector: 'app-users-list-content',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users-list-content.component.scss']
 })
 export class UsersListContentComponent implements OnInit {
+  users: User[] = [];
+  selectedUser: User;
 
-  constructor() { }
+  constructor(private usersService: UsersService, private notificationService: NotificationService<String>) { }
 
   ngOnInit() {
+    this.usersService.getUsers().subscribe((users) => {
+      this.users = users;
+      console.log(this.users);
+    });
+    this.notificationService.notifier.subscribe((val) => {
+      if (typeof val === 'boolean') {
+        this.selectedUser = null;
+      } else {
+        const selectedUserId: number = +val;
+        this.selectedUser = this.users.find((user) => {
+          return user.id === +selectedUserId;
+        });
+      }
+    });
   }
-
 }
--- src/app/users/ui/users-list-header/users-list-header.component.ts
+++ src/app/users/ui/users-list-header/users-list-header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { NgForm } from '@angular/forms';
+import { UsersService } from '../../users.service';
 
 @Component({
   selector: 'app-users-list-header',
@@ -7,12 +9,10 @@ import { faPlus } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./users-list-header.component.scss']
 })
 export class UsersListHeaderComponent implements OnInit {
-
   faPlus = faPlus;
 
-  constructor() { }
+  constructor(private usersService: UsersService) { }
 
   ngOnInit() {
   }
-
 }
