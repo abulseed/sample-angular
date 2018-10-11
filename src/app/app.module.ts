964444474cac044b6825348b155c13281cf45355
Ahmed elSayed <aelsayed.mo@gmail.com>
2018-10-11 03:23:54 +0200

    Add NgRx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LogoComponent } from './logo/logo.component';
+import { StoreModule } from '@ngrx/store';
 
 import { AuthenticationModule } from './auth/authentication.module';
 import { UsersModule } from './users/users.module';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { selectedUserReducer } from './users/store/users.reducers';
 
 @NgModule({
   imports: [
     BrowserModule,
     UsersModule,
     AuthenticationModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot({ selectUser: selectedUserReducer })
   ],
   declarations: [
     AppComponent,
